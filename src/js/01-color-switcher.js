809e955c7560ed24af9675f8aa0f0b02f923f513
Vadym <besarabvadim2@gmail.com>
2022-11-03 15:46:11 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+
+let intervalID = null;
+
+startButton.addEventListener("click", onStartButtonClik);
+stopButton.addEventListener("click", onStopButtonClik);
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function onStartButtonClik() {
+
+    intervalID = setInterval(() => {
+
+         document.body.style.backgroundColor = getRandomHexColor();
+     }, 1000);
+     
+};
+console.log(onStartButtonClik);
+
+function onStopButtonClik() {
+    clearInterval(intervalID);
+    };
+
+console.log(onStopButtonClik);
