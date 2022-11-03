362030d7659850cd2842af64d6dfc0ab9e4a1360
vadym-bess <95138585+vadym-bess@users.noreply.github.com>
2022-11-03 14:43:57 +0000

    Deploying to gh-pages from @ vadym-bess/goit-js-hw-09@e3743d414599536e6fe46607c62811ca375010cc 🚀

---
+++ 01-color-switcher.8a2a1208.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");t.addEventListener("click",(function(){e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){clearInterval(e)}));var e=null}();
+//# sourceMappingURL=01-color-switcher.8a2a1208.js.map
